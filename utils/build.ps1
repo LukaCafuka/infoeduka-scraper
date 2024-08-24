4b1d0e3845f64439d902e090500d794011f10c03
Luka <87535465+LuxanL@users.noreply.github.com>
2024-08-24 20:52:59 +0200

    Removed duplicate startScrape function and joined them together

--- utils/build.ps1
+++
@@ -1,2 +0,0 @@
-pyinstaller --onefile --icon=icon.ico .\infoedukascrape.py
-pyinstaller --onefile --windowed --icon=icon.ico --add-data "icon.png;." .\infoedukascrape_gui.py
--- utils/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-pyinstaller --onefile --icon=icon.ico .\infoedukascrape.py
-pyinstaller --onefile --windowed --icon=icon.ico --add-data "icon.png;." .\infoedukascrape_gui.py
